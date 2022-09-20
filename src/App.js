baba3c753ad0c110abe572e4b04403cd34b6bb51
Shenol Osman <7887878+shenolosman@users.noreply.github.com>
2022-09-20 21:42:29 +0200

    Routing inital pages

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
-
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/home/Home";
+import Login from "./pages/login/Login";
+import Signup from "./pages/signup/Signup";
 
 function App() {
   return (
     <div>
-    
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/pages/login/Login.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import "./Login.module.css"
+const Login = () => {
+    return (
+        <div>
+           Login 
+        </div>
+    );
+}
+
+export default Login;
