0285be1940e8e8f8448cf4af029d3929308f9d72
Shenol Osman <7887878+shenolosman@users.noreply.github.com>
2022-09-20 22:21:46 +0200

    login page created

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,11 +1,36 @@
-import React from 'react';
-import "./Login.module.css"
+import {
+  Container,
+  Typography,
+  Button,
+  FormControl,
+  FilledInput,
+  InputLabel,
+} from "@mui/material";
+import React from "react";
+import "./Login.module.css";
 const Login = () => {
-    return (
-        <div>
-           Login 
-        </div>
-    );
-}
+  return (
+    <Container>
+      <form>
+        <Typography
+          sx={{ mt: 15,textAlign:"center", fontWeight: "bold" }}
+          variant="h2"
+          color="darkslateblue"
+        >
+          Login Page
+        </Typography>
+        <FormControl fullWidth sx={{ mt: 5 }}>
+          <InputLabel htmlFor="email">Email</InputLabel>
+          <FilledInput id="email" label="Email" />
+        </FormControl>
+        <FormControl fullWidth sx={{ my: 5 }}>
+          <InputLabel htmlFor="password">Password</InputLabel>
+          <FilledInput id="password" label="Password" />
+        </FormControl>
+        <Button variant="outlined" type="submit" color="primary" size="large" sx={{mt:1}}>Log in</Button>
+      </form>
+    </Container>
+  );
+};
 
 export default Login;
